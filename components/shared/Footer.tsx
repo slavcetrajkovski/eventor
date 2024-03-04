53c2f146ba5a30458d61d46a0c939f1f1d7bf5a3
Slavcho <s.trajkovski02@yahoo.com>
2024-03-04 15:47:39 +0100

    Moongose and MongoDB setup

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={30}
+          />
+        </Link>
+
+        <p>2024 Eventor. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
