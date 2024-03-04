ef33ed66bb483dbc7d74117865989c96bd31f8cd
Slavcho <s.trajkovski02@yahoo.com>
2024-03-04 19:54:53 +0100

    Setting up the Create Event page

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "eventor_db",
+      dbName: "eventor",
       bufferCommands: false,
     });
 
