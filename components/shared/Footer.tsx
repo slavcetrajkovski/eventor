94773de44fd5c6c62a0173ac297541049777ac45
Slavcho <s.trajkovski02@yahoo.com>
2024-09-11 17:15:33 +0200

    footer implementation

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
 
-export default Footer
+        <p>
+          2024 Eventor. Project developed by Slavko Trajkovski for subject
+          Advanced Web Design.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
